3294866587bb45ab372f4fad3a144b5c8297d244
mz02005 <mz02005@qq.com>
2016-05-06 23:56:05 +0800

    继续增加测试用例用于测试for语句

---
+++ test/script/forstatement.c
@@ -0,0 +1,15 @@
+int i;
+for (;i<100;i+=1);
+
+int cycle = 100000;
+for (i=1;;i+=1)
+{
+	if (i >= 10000000)
+		break;
+	float v = i;
+	v /= cycle;
+	if (v == i / cycle)
+		println(i);
+}
+
+println("forstatement finished\n\n");
