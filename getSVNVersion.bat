c0211d046dba3e82ee3b9b1d8819cec2253e6756
mz02005 <mz02005@qq.com>
2016-07-16 14:29:26 +0800

    1.修复无svn时的编译错误问题

--- getSVNVersion.bat
+++ getSVNVersion.bat
@@ -1,7 +1,7 @@
 @echo off
 set SVNINFO_PATHNAME=svnInfo.h
 svn info > vtemp.txt
-if %errorlevel% NEQ 0 (echo "get svn info fail"&& exit 1)
+if %errorlevel% NEQ 0 (echo "get svn info fail"&& goto svnError)
 for /f "delims=: tokens=1,2" %%i in (vtemp.txt) do (
  if "%%i" EQU "Revision" (
   echo Found revision is %%i
@@ -11,6 +11,7 @@ for /f "delims=: tokens=1,2" %%i in (vtemp.txt) do (
  )
 )
 
+:svnError
 echo Could not find revision information
 echo #pragma once > %SVNINFO_PATHNAME%
 echo #define SVN_VERSION -1 >> %SVNINFO_PATHNAME%
